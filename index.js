a86db6381bae337d18a3a7b21378d60151c7d34b
Cristina Lynn <cristina@cristinalynn.net>
2023-03-01 20:14:08 -0800

    Write functions getButton, getDogs, showDogs in index.js

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    getDogs();
+    
+    getButton();
 })
+function getButton() {
+const allDogsButton = document.getElementById("all")
+    allDogsButton.addEventListener("click", () => { getDogs()})
+}
+
+
 
 function getDogs() {
-    
+    fetch('http://localhost:3000/dogs') 
+    .then(response => response.json())
+    .then(data => showDogs(data))
 }
+
+function showDogs(dogs) {
+    dogs.forEach(dog => { 
+        const h2 = document.createElement("h2")
+        h2.innerText = dog.name
+       const dogContainer = document.getElementById("dogs-container")
+        const img = document.createElement("img")
+        img.src = dog.image
+        img.className = "dog-img"
+        dogContainer.append(h2, img)
+        
+    })
+}
+
+
+ 
