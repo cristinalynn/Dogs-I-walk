2d4a2d38ab86507adb896c448c18f8ab16136a01
Genevieve <genevieve@1degree.org>
2023-03-17 18:21:27 -0700

    Made suggestions

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
 const dogs = []
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:3000/dogs') 
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/dogs")
     .then(response => response.json())
     .then(data => getDogs(data))
    
-    const searchForm = document.getElementById("search-form");
+    const searchForm = document.getElementById("search-form")
     searchForm.addEventListener("submit", (e) => {searchDogs(e)})    
    
     const allDogsButton = document.getElementById("all")
-    allDogsButton.addEventListener("click", () => {showAllDogs()})
-    
+    allDogsButton.addEventListener("click", () => {
+        const dc = document.getElementById("dogs-container")
+        if (dc.children.length == 0) {
+            showAllDogs()
+        }
+    })
 })
 
-function getDogs(data) {
-    data.forEach((dog) => dogs.push(dog))
-    
+function getDogs(dogData) {
+    dogData.forEach((dog) => dogs.push(dog))
 }
 
 function searchDogs(e) {
-    e.preventDefault();
-    const findDogs = dogs.filter(dog => dog.dogBreed === e.target.dogBreed.value)
-    findDogs.forEach((dog) => {
-        const h2 = document.createElement("h2");
-        h2.innerText = dog.name;
-        const dogContainer = document.getElementById("searchByDogBreed_container")
-        const img = document.createElement("img")
-        img.src = dog.image
-        img.className = "dog-img"
-        dogContainer.append(h2, img)
-    })
+    e.preventDefault()
+    const dogsOfBreed = dogs.filter(dog => dog.dogBreed === e.target.dogBreed.value)
+    const dogBreedContainer = document.getElementById("searchByDogBreed_container")
+    dogsOfBreed.forEach((dog) => {createDogElement(dog, dogBreedContainer)})
 }
 
 function showAllDogs() {
-    dogs.forEach(dog => { 
-        dogs.push(dog)
-        const h2 = document.createElement("h2")
-        h2.innerText = dog.name
-       const dogContainer = document.getElementById("dogs-container")
-        const img = document.createElement("img")
-        img.src = dog.image
-        img.className = "dog-img"
-        dogContainer.append(h2, img)
-        
-    }
-    );
+    const allDogsContainer = document.getElementById("dogs-container")
+    dogs.forEach(dog => {createDogElement(dog, allDogsContainer)})
 }
 
-
-
-
-
-
+function createDogElement(dog, containerName) {
+    const h2 = document.createElement("h2")
+    h2.innerText = dog.name
+    const img = document.createElement("img")
+    img.src = dog.image
+    img.className = "dog-img"
+    containerName.append(h2, img)
+}
