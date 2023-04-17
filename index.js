31c55c68c7c4a70e69d9a3ce4bf658ded989ff76
Cristina Lynn <cristina@cristinalynn.net>
2023-04-17 16:04:21 -0700

    Update db.json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:3000/dogs")
     .then(response => response.json())
     .then(data => getDogs(data))
+
    
     const searchForm = document.getElementById("search-form")
     searchForm.addEventListener("submit", (e) => {
@@ -10,14 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
         if (dbc.children.length == 0) {
         searchDogs(e)
         }
-    })    
+    })  
    
     const allDogsButton = document.getElementById("all")
     allDogsButton.addEventListener("click", () => {
         const dc = document.getElementById("dogs-container")
+        //if the length of the dog container's children is 0, then show all dogs 
         if (dc.children.length == 0) {
             showAllDogs()
         }
+        
     })
 })
 
@@ -33,6 +36,7 @@ function searchDogs(e) {
 }
 
 function showAllDogs() {
+    
     const allDogsContainer = document.getElementById("dogs-container")
     dogs.forEach(dog => {createDogElement(dog, allDogsContainer)})
 }
@@ -44,4 +48,14 @@ function createDogElement(dog, containerName) {
     img.src = dog.image
     img.className = "dog-img"
     containerName.append(h2, img)
+    //const dogImages = document.querySelectorAll(".dog-img")
+    // console.log(dogImages)
+    // dogImages.addEventListener('click', function() {
+    //     console.log('hi')
+    //})
 }
+
+
+
+// add a click event listener to the dog images
+// create a const named dogImages and add the click event listener to it
